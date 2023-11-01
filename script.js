b03ff63670442ecfc1c8410e144ba737841d4675
khaticaa <128252286+khaticaa@users.noreply.github.com>
2023-11-02 02:24:35 +0400

    first js task

---
+++ script.js
@@ -0,0 +1,36 @@
+// DATA1
+// 1)
+let nihadBoy = 1.69;
+let nihadCeki = 78;
+let nailBoy = 1.95;
+let nailCeki = 92;
+
+// 2)
+nihadBk = nihadCeki / nihadBoy ** 2;
+nailBk = nailCeki / nailBoy ** 2;
+
+// 3)
+if (nihadBk > nailBk) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// DATA2
+
+// 1)
+let NihadBoy2 = 1.88;
+let NihadCeki2 = 95;
+let NailBoy2 = 1.76;
+let NailCeki2 = 85;
+
+// 2)
+NihadBk2 = NihadCeki2 / NihadBoy2 ** 2;
+NailBk2 = NailCeki2 / NailBoy2 ** 2;
+
+// 3)
+if (NihadBk2 > NailBk2) {
+  console.log(true);
+} else {
+  console.log(false);
+}
